b7cbc293d266a2666df0f138ea6a3595782f14b9
Akintola Olamilekan <akintolaolamilekan51@gmail.com>
2025-12-13 23:33:39 +0100

    build: database models created

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,11 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 // Load environment-specific file
-const envFile =
-  process.env.NODE_ENV === 'production'
-    ? '.env.production'
-    : '.env.development';
+const envFile = process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev';
 
-dotenv.config({ path: path.resolve(__dirname, `../${envFile}`) });
+dotenv.config({ path: path.resolve(__dirname, `./${envFile}`) });
+
+console.log(process.env.DATABASE_URL);
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
